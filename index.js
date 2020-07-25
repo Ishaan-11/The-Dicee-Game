9a7c935b2734227a747435a4ec4e55b0e3d27dc6
Ishaan-11 <ishaansharma1998@gmail.com>
2020-07-25 17:23:06 +0530

    angela's solution

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-var randomNumber1 = Math.floor(Math.random() * 6) +1;
-document.querySelector(".img1").setAttribute("src","images/dice" + randomNumber1 + ".png");
+var randomNumber1 = Math.floor(Math.random() * 6) +1;//1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";//dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage;//images/dice1.png - images/dice6.png 
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+// document.querySelector(".img1").setAttribute("src","images/dice" + randomNumber1 + ".png");
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
 
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";  
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+// document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
+
+//if player 1 wins
 if(randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "🚩Player 1 Wins";    
-} else if(randomNumber1 < randomNumber2) {
-    document.querySelector("h1").textContent = "Player 2 Wins🚩";
-} else {
-    document.querySelector("h1").textContent = "Draw!";
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+    // document.querySelector("h1").textContent = "🚩Player 1 Wins";
+}
+else if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+    // document.querySelector("h1").textContent = "Player 2 Wins🚩";
+} 
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+    // document.querySelector("h1").textContent = "Draw!";
 }
