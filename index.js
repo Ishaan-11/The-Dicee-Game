3bd6489cfea5a13f08c72e6670e369137a761d14
Ishaan-11 <ishaansharma1998@gmail.com>
2020-12-26 13:06:01 +0530

    step 5

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = getRandomNumber();
+var randomNumber2 = getRandomNumber();
 var img1 = document.querySelector(".img1");
+var img2 = document.querySelector(".img2");
 
 //set random image for img1
 img1.setAttribute("src", `images/dice${randomNumber1}.png`);
+
+//set random image for img1
+img2.setAttribute("src", `images/dice${randomNumber2}.png`);
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 6) + 1;
+}
