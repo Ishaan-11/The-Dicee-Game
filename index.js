264422f185e7237dee82c98c918a523f404547d5
Ishaan-11 <ishaansharma1998@gmail.com>
2020-12-26 13:04:30 +0530

    step 4

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber1);
+var img1 = document.querySelector(".img1");
+
+//set random image for img1
+img1.setAttribute("src", `images/dice${randomNumber1}.png`);
